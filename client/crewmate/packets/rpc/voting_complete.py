e8be6b8daf7860b4c6483f814ec989ff55c35fd3
Tominous <18189490+Tominous@users.noreply.github.com>
2020-10-15 17:51:09 +0100

    Update voting_complete.py

--- client/crewmate/packets/rpc/voting_complete.py
+++ client/crewmate/packets/rpc/voting_complete.py
@@ -4,14 +4,14 @@ from scapy.packet import Packet
 class VotingCompleteRPC(Packet):
     name = "VotingCompleteRPC"
     fields_desc = [
-        # LEShortField("statesLen", None),
-        # StrLenField(
-        #     "statesArray",
-        #     None,
-        #     length_from=lambda packet: packet.playerNameLen
-        # ),
-        # ByteField("playerId", None),  # 0xFF if nobody
-        # ByteField("votingTie", None),
+         LEShortField("statesLen", None),
+         StrLenField(
+             "statesArray",
+             None,
+             length_from=lambda packet: packet.playerNameLen
+         ),
+         ByteField("playerId", None),  # 0xFF if nobody
+         ByteField("votingTie", None),
     ]
 
     def extract_padding(self, p):
